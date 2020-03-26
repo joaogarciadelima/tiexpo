6eb7f5d0c217f519891321bb8dda16a6cd36dfba
João garcia <joaogarciadelimaneto@gmail.com>
2020-03-26 15:38:29 -0300

    Alterada home e melhorado form de login closes #39

--- tiexpo/urls.py
+++ tiexpo/urls.py
@@ -16,11 +16,22 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordResetCompleteView, PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contas/', include('django.contrib.auth.urls')),
+    path('contas/login/', LoginView.as_view(), name='login'),
+    path('contas/logout/', LogoutView.as_view(), name='logout'),
+    path('contas/reiniciar_senha/', PasswordResetView.as_view(), name='password_reset'),
+    path('contas/reiniciar_senha/ok', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('contas/reiniciar/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('contas/reiniciar/ok', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('tiexpo.base.urls')),
     path('albuns/', include('tiexpo.albuns.urls'), name='albuns'),
     path('api/v1/', include('tiexpo.api.urls'), name='api'),
