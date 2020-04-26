3ed2043e67895e63c80d536fcf1328455f241be6
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-04-26 10:25:48 -0300

    Added manifest

--- tiexpo/base/urls.py
+++ tiexpo/base/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from tiexpo.base.views import home
 
@@ -6,4 +7,8 @@ from tiexpo.base.views import home
 app_name = 'base'
 urlpatterns = [
     path('', home, name='home'),
+    path('service_worker.js', (
+        TemplateView.as_view(
+            template_name="base/service_worker.js",
+            content_type='application/javascript', )), name='service_worker.js'),
 ]
