7cc8cdcc07d1bc12d9527279abd52ac56db0c25d
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-06-18 14:00:17 -0300

    Ajustados templates

--- tiexpo/base/urls.py
+++ tiexpo/base/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from tiexpo.base.views import home
 
@@ -7,8 +6,4 @@ from tiexpo.base.views import home
 app_name = 'base'
 urlpatterns = [
     path('', home, name='home'),
-    path('service_worker.js', (
-        TemplateView.as_view(
-            template_name="base/service_worker.js",
-            content_type='application/javascript', )), name='service_worker.js'),
 ]
