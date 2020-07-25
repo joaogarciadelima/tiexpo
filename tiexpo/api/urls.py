160ab208514987d5a430d341ea992190884940fe
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-07-25 11:46:53 -0300

    Alterada api - Limit Offset

--- tiexpo/api/urls.py
+++ tiexpo/api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.catalogo_list),
     path('imagens/', views.imagem_list),
     path('imagens/<int:id>/', views.imagem_detail),
+    # path('imagens/catalogos/<str: titulo>', views.catalogo_imagens_list),
+    # path('imagens/fabricantes/', views.imagem_detail),
 ]
