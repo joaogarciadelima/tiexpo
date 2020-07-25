b2170302a77a4d30165ab262c863047707e307fb
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-07-25 11:46:53 -0300

    Alterada api - Limit Offset

--- tiexpo/settings.py
+++ tiexpo/settings.py
@@ -54,9 +54,12 @@ CORS_ORIGIN_ALLOW_ALL = True
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 25,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+
 }
 
 MIDDLEWARE = [
