d9589171585fd354ce6f006b5b0544d682fed8cd
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-08-01 12:02:40 -0300

    Fixing Json

--- tiexpo/settings.py
+++ tiexpo/settings.py
@@ -55,6 +55,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'PAGE_SIZE': 100,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'DEFAULT_PERMISSION_CLASSES': [
