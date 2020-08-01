875e47c270d2579953a058bd5c01cd157538000c
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-08-01 18:42:00 -0300

    Change API checking security

--- tiexpo/api/urls.py
+++ tiexpo/api/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from tiexpo.api import views
 
 urlpatterns = [
-    path('', views.CatalogoList.as_view()),
-    path('imagens/', views.ImageList.as_view()),
+    path('', views.ImageList.as_view()),
+    path('imagens/', views.ImageApiView.as_view()),
     path('imagens/<int:pk>/', views.ImageDetail.as_view()),
     path('imagens/catalogos/', views.CatalogoList.as_view()),
-    # path('imagens/catalogos/<int:catalogo_id>', views.highlight),
 ]
