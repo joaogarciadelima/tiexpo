88f4b2511a925c78ff0c5eb342d929686564f5a3
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-09-18 11:52:03 -0300

    Adicionada politica de privacidade básica no site

--- tiexpo/base/urls.py
+++ tiexpo/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from tiexpo.base.views import home
+from tiexpo.base.views import home, privacy
 
 
 app_name = 'base'
 urlpatterns = [
     path('', home, name='home'),
+    path('privacidade/', privacy, name='privacy'),
 ]
